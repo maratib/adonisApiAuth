322478827efb5356160af657357e7ae57066b4a6
admin <admin@imac.local>
2020-04-29 19:26:14 +0500

    page controller added

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -1,22 +1,26 @@
-'use strict'
+'use strict';
 const Page = use('App/Models/Page');
 
 class PageController {
-    async index({request, response, locale}) {
+    async index({ request, response, locale }) {
         const pageData = request.only(['lang', 'page']);
         console.log(pageData);
-        
-        const where = {lang: pageData.lang, name: pageData.page};
+
+        const where = { lang: pageData.lang, name: pageData.page };
         const page = await Page.query().select('title', 'body').where(where).first();
-        console.log(page);
+        // console.log(page);
         if (page !== null) return page;
-        else return {title:'na', body: 'na'};
-        
-        
+        else {
+            const page = await Page.query().select('title', 'body').where('name', pageData.page).first();
+            if (page !== null) return page;
+            return { title: '', body: '' };
+        }
+
+
     }
     async create({ request, auth, response }) {
 
     }
 }
 
-module.exports = PageController
+module.exports = PageController;
